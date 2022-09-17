2b3f6826ada6a2f47285c0889bb9f9b20896b49d
EmmanuelVictor62 <104584134+EmmanuelVictor62@users.noreply.github.com>
2022-09-17 18:11:32 +0100

    Added Pictures

--- airbnb/src/components.js/ProfileData.js
+++ airbnb/src/components.js/ProfileData.js
@@ -14,18 +14,18 @@ import Image13 from '../images/img-13.webp'
 import Image14 from '../images/img-14.webp'
 import Image15 from '../images/img-15.webp'
 import Image16 from '../images/img-16.webp'
-import Image17 from '../images/img-1.webp'
-import Image18 from '../images/img-1.webp'
-import Image19 from '../images/img-1.webp'
-import Image20 from '../images/img-1.webp'
-import Image21 from '../images/img-1.webp'
-import Image22 from '../images/img-1.webp'
-import Image23 from '../images/img-1.webp'
-import Image24 from '../images/img-1.webp'
-import Image25 from '../images/img-1.webp'
-import Image26 from '../images/img-1.webp'
-import Image27 from '../images/img-1.webp'
-import Image28 from '../images/img-1.webp'
+import Image17 from '../images/img-17.webp'
+import Image18 from '../images/img-18.webp'
+import Image19 from '../images/img-19.webp'
+import Image20 from '../images/img-20.webp'
+import Image21 from '../images/img-21.webp'
+import Image22 from '../images/img-22.webp'
+import Image23 from '../images/img-23.webp'
+import Image24 from '../images/img-24.webp'
+import Image25 from '../images/img-25.webp'
+import Image26 from '../images/img-26.webp'
+import Image27 from '../images/img-27.webp'
+import Image28 from '../images/img-28.webp'
 
 export default [
     {
@@ -46,21 +46,21 @@ export default [
         id:2,
         link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
         profileImg: SecondImage,
-        country:"Accra, Ghana",
+        country:"La Quinta, Spain",
         stats: {
             icon:<i class="bi bi-star-fill"></i>,
             rating:"4.68",
-            distance:"3,100 kilometers",
-            date:"Sep 16-21"
+            distance:"2,100 kilometers",
+            date:"Aug 18-21"
         },
-        amount:"79",
+        amount:"510",
         available:"Available"
     },
     {
         id:3,
         link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
         profileImg:Image3,
-        country:"Accra, Ghana",
+        country:"Cairo, Egypt",
         stats: {
             icon:<i class="bi bi-star-fill"></i>,
             rating:"4.68",
@@ -83,5 +83,341 @@ export default [
         },
         amount:"79",
         available:"Available"
+    },
+    {
+        id:5,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image5,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:6,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image6,
+        country:"London, United Kingdom",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"5.00",
+            distance:"4,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"520",
+        available:"Available"
+    },
+    {
+        id:7,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image7,
+        country:"Porto, Germany",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.98",
+            distance:"3,100 kilometers",
+            date:"Sep 19-21"
+        },
+        amount:"220",
+        available:"Available"
+    },
+    {
+        id:8,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image8,
+        country:"Vindax, Rico",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"3.68",
+            distance:"4,200 kilometers",
+            date:"Oct 4-21"
+        },
+        amount:"120",
+        available:"Not Available"
+    },
+    {
+        id:9,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image9,
+        country:"Todax, Tame",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"290",
+        available:"Available"
+    },
+    {
+        id:10,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image10,
+        country:"Abuja, Nigeria",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:11,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image11,
+        country:"Lagos, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"1000",
+        available:"Available"
+    },
+    {
+        id:12,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image12,
+        country:"Pindax, Taoome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"110",
+        available:"Available"
+    },
+    {
+        id:13,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image13,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:14,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image14,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:15,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image15,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:16,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image16,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:17,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image17,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:18,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image18,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:19,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image19,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:20,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image20,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:21,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image21,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:22,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image22,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:23,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image23,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:24,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image24,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:25,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image25,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:26,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image26,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:27,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image27,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
+    },
+    {
+        id:28,
+        link:"https://www.airbnb.com/rooms/49085118?adults=1&category_tag=Tag%3A7769&children=0&infants=0&search_mode=flex_destinations_search&check_in=2022-09-16&check_out=2022-09-21&previous_page_section_name=1000&federated_search_id=39d5d58e-b685-4e8c-8082-31ebada16048",
+        profileImg:Image28,
+        country:"Togo, Taome",
+        stats: {
+            icon:<i class="bi bi-star-fill"></i>,
+            rating:"4.68",
+            distance:"3,100 kilometers",
+            date:"Sep 16-21"
+        },
+        amount:"790",
+        available:"Not Available"
     },
 ]
